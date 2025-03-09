b770a01546dabb5163edce8b969366296cc8587f
Sidhin S Thomas <thomas.sidhin@outlook.com>
2025-03-09 09:21:45 +0530

    Remove drop choice - standard implementation available

--- app/src/main/java/in/sidhin/app/budgetbud/ui/DropChoice.kt
+++
@@ -1,9 +0,0 @@
-package `in`.sidhin.app.budgetbud.ui
-
-import android.graphics.drawable.Icon
-import androidx.compose.runtime.Composable
-
-data class DropChoiceItem(val displayName: String, val id: String, val icon: Icon? = null)
-
-@Composable
-fun DropChoice(itemList: List<DropChoiceItem>, selectionChanged:((String) -> Unit) ) {}
